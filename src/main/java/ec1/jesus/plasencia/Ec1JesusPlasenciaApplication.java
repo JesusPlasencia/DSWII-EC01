b951f817a82318cbd5d1be52307226562efcaf4a
Jesus Toledo <jesusplasencia2508@hotmail.com>
2022-10-04 19:16:56 -0500

    Commit Init

---
+++ src/main/java/ec1/jesus/plasencia/Ec1JesusPlasenciaApplication.java
@@ -0,0 +1,13 @@
+package ec1.jesus.plasencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1JesusPlasenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1JesusPlasenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec1/jesus/plasencia/Ec1JesusPlasenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec1.jesus.plasencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec1JesusPlasenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
